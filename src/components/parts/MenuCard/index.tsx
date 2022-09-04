ef409ffd005baa0a163843d93e8bb8c445116642
koichi-menta <101hundredone@gmail.com>
2022-09-04 21:15:30 +0900

    メインメニューを作成

---
+++ src/components/parts/MenuCard/index.tsx
@@ -0,0 +1,82 @@
+import React from "react";
+import styled from "styled-components";
+
+export type ContainerProps = {
+  onClick: () => void;
+};
+
+type Props = {
+  className?: string;
+} & ContainerProps;
+
+const Component = ({ className }: Props): JSX.Element => (
+  <div className={className}>
+    <div className="background"></div>
+    <p className="text">K</p>
+  </div>
+);
+
+const StyledComponent = styled(Component)`
+  height: 100%;
+  width: 100%;
+  max-width: 100px;
+  max-height: 100px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 20px;
+  position: relative;
+  background-color: #fa9;
+  overflow: hidden;
+  cursor: pointer;
+  > .background {
+    animation-name: animation-sample;
+    animation-duration: 1s;
+    animation-direction: normal;
+    animation-iteration-count: infinite;
+    animation-timing-function: linear;
+    width: 200%;
+    height: 200%;
+    position: absolute;
+    z-index: 0;
+    background: linear-gradient(
+      to right bottom,
+      red,
+      orange,
+      yellow,
+      green,
+      blue,
+      red,
+      orange,
+      yellow,
+      green,
+      blue
+    );
+  }
+  > .text {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-size: 65px;
+    width: calc(100% - 5px);
+    height: calc(100% - 5px);
+    margin: 0;
+    padding: 0;
+    position: relative;
+    z-index: 1;
+    background-color: #fff;
+    border-radius: 20px;
+  }
+  @keyframes animation-sample {
+    0% {
+      transform: rotate(0deg);
+    }
+    100% {
+      transform: rotate(360deg);
+    }
+  }
+`;
+
+export const MenuCard = (props: ContainerProps): JSX.Element => {
+  return <StyledComponent {...props} />;
+};
